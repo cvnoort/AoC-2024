2caf89b7bbd77d38a6af9e7266adbb0c5e84433f
Charlotte van Noort <61543485+cvnoort@users.noreply.github.com>
2024-12-02 15:21:13 +0100

    Day 2 Part 1 (Python)

---
+++ day2.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+def check_report(report):
+
+    direction = "unknown"
+
+    for i, level in enumerate(report[1:], start=1):
+
+        if abs(level - report[i-1]) not in range(1,4):
+            safe = False
+            break
+
+        if level > report[i-1]:
+            if direction == "decreasing":
+                safe = False 
+                break
+            direction = "increasing"
+        elif level < report[i-1]:
+            if direction == "increasing":
+                safe = False 
+                break
+            direction = "decreasing"
+        elif level == report[i-1]:
+            safe = False
+            break
+
+        safe = True
+    
+    return safe
+
+with open("input.txt") as file: 
+    safe_checks = []
+    for line in file:
+        report_vals = list(map(int, line.split())) 
+        safe_checks.append(check_report(report_vals))
+    
+    print("Number of safe reports:", safe_checks.count(True))

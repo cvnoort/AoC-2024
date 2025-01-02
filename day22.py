5c39d864d22a6d085bd387f81ebc101d445ed1bc
Charlotte van Noort <61543485+cvnoort@users.noreply.github.com>
2025-01-02 20:59:10 +0100

    Day 22 Part 2 (Python)

--- day22.py
+++ day22.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3 
 
+################################### part 1 ###################################
+
 def mix(number:int, operation:str) -> int:
     "Mix a new number into the secret number."
     new_number = eval(f"{number} {operation}")
@@ -30,3 +32,27 @@ for secret_number in initial_numbers:
 
 print("Sum of 2000th secret number for each buyer:", 
       sum(twothousandth_numbers))
+
+################################### part 2 ###################################
+
+price_dict = {}
+for secret_number in initial_numbers:
+    price_changes = []
+    buyer_dict = {}
+    # Get prices/price changes for a buyer's set of secret numbers
+    for i in range(2000):
+        prev_number = secret_number
+        secret_number = get_next_secret_num(secret_number)
+        price = str(secret_number)[-1]
+        change = int(str(prev_number)[-1]) - int(price)
+        price_changes.append(change)
+        if len(price_changes) > 4:
+            price_changes.pop(0)
+            buyer_dict.setdefault(str(price_changes), int(price))
+    # Update total bananas for each sequence of four price changes
+    for sequence in buyer_dict:
+        price_dict.setdefault(sequence, 0)
+        price_dict[sequence] += buyer_dict[sequence] 
+    
+print("Maximum number of bananas:", 
+      price_dict[max(price_dict.keys(), key=(lambda k: price_dict[k]))]) 
